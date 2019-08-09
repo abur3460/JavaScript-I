f53179d7b30e6a8ce13ad9a3d166ed9ea93bb841
Austin Burton <abur3460@gmail.com>
2019-08-08 17:22:50 -0700

    finished function-conversion, finished objects.js, I am finishedgit add .

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,24 +1,36 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
-
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
 // myFunction();
+
+let myFunction = param => console.log("This Funcion was invoked!")
+myFunction();
+
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
+let anotherFunction = param => console.log(param)
+anotherFunction("Example");
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (a, b) => a + b;
+add(1, 2);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+let subtract = (a, b) => a - b;
+subtract(1,2);
 
 
 // Stretch
